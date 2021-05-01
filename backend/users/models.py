d4b2bf91539539145ec85e721dfa1bee21385110
abdellahia <abdellah_idrissiazami@um5.ac.ma>
2021-05-01 01:57:10 +0000

    add classe

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+class User (AbstractBaseUser):
+    email = models.EmailField(verbose_name = 'email', max_Length=60, unique = True)
+    username = models.CharField(max_Length=30, unique = True)
+    
